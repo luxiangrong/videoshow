500bfc0490a5be412e0b19c3fb1f654d778371fc
LuXiangrong <luxiangrongwave@gmail.com>
2015-04-19 19:40:21 +0800

    换了个视频

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	var movieWidth = 612,
-		movieHeight = 296,
+	var movieWidth = 622,
+		movieHeight = 354,
 		winWidth = $(window).width() - 30,
 		winHeight = $(window).height() - 30;
 
@@ -9,10 +9,6 @@ $(document).ready(function() {
 
 	var rate = widthRate < heightRate ? widthRate : heightRate;
 
-	console.log(movieWidth * rate);
-	console.log(movieHeight * rate);
-
-
     var jPlayer = $("#jquery_jplayer_1").jPlayer({
         ready: function() {
             $(this).jPlayer("setMedia", {
@@ -20,7 +16,7 @@ $(document).ready(function() {
                 m4v: "images/building.mp4",
             });
 
-            $(this).jPlayer("pause", 0.01);
+            $(this).jPlayer("pause", 0.1);
         },
         size: {
         	width: movieWidth,
@@ -37,59 +33,46 @@ $(document).ready(function() {
     });
 
     $("#jquery_jplayer_1").bind($.jPlayer.event.timeupdate , function(event) { // Add a listener to report the time play began
-  		if (event.jPlayer.status.currentTime > pauseTimes[currentFrame].time) {
-         	$(this).jPlayer('pause');
-         	$('.text').addClass('in');
-         	$('.text').text(pauseTimes[currentFrame].text);
-         	$('.text').css('color', pauseTimes[currentFrame].color);
-         	currentFrame ++;
-
-      	}
+  		if(currentFrame < pauseTimes.length) {
+            if (event.jPlayer.status.currentTime > pauseTimes[currentFrame].time) {
+             	$(this).jPlayer('pause');
+             	currentFrame ++;
+                console.log(currentFrame);
+          	}
+          } 
 	});
 
     var currentFrame = 0;
     var pauseTimes = [
     	{
-    		time: 10,
-    		text: '房山优质汉白玉',
-    		color: '#365F91',
+    		time: 3,
     		transition: 'slide-from-left'
     	},
     	{
-    		time: 15,
-    		text: '整个大殿都采用榫卯结构',
-    		color: '#800000',
+    		time: 7,
     		transition: 'slide-from-right'
     	},
     	{
-    		time: 25,
-    		text: '大殿坐北朝南，尊享皇家尊严',
-    		color: '#365F91',
+    		time: 27,
     		transition: 'slide-from-left'
     	},
     	{
-    		time: 31,
-    		text: '大殿前后落差9尺',
-    		color: '#800000',
+    		time: 46,
     		transition: 'slide-from-right'
-    	},
-    	{
-    		time: 50,
-    		text: '景山山顶可以俯瞰故宫',
-    		color: '365F91',
-    		transition: 'slide-from-left'
     	}
-
     ];
 
     $('.text').addClass('fade in');
     $('.text').addClass('slide-from-left');
     $(document).on('click', function(){
-    	$('.text').removeClass('in');
+        $('.intro-' + (currentFrame)).addClass('out').removeClass('in');
+        $('.intro-' + (currentFrame + 1)).addClass('in').removeClass('out');
     	$("#jquery_jplayer_1").jPlayer('play');
-    	if(currentFrame > 0) {
-    		$('.text').removeClass(pauseTimes[currentFrame - 1].transition);
-    	}
-    	$('.text').addClass(pauseTimes[currentFrame].transition);
+        if(currentFrame == 4) {
+            $("#jquery_jplayer_1").jPlayer('play', 0.1);
+            currentFrame = 0;
+        }
+        $('.intro-' + (currentFrame)).addClass('out').removeClass('in');
+        $('.intro-' + (currentFrame + 1)).addClass('in').removeClass('out');
     });
 });
